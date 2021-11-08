066390bfacde47a285ccdb24cb7946ed9b0c261a
Shih Han Wang <shih-han.wang@boxt.co.uk>
2021-11-08 18:46:35 +0000

    Retrun test panel object json

--- app/controllers/test_panels_controller.rb
+++ app/controllers/test_panels_controller.rb
@@ -1,7 +1,12 @@
 class TestPanelsController < ApplicationController
 
   def show
-    render json: {}
+    render json: test_panel
   end
 
+  private
+
+  def test_panel
+    @test_panel ||= TestPanel.find_by_id(params[:id])
+  end
 end
--- app/models/test_panel.rb
+++ app/models/test_panel.rb
@@ -2,18 +2,22 @@ class TestPanel
   DATA = [
     {
       id: 'TP1',
-      tests: ['CHO', 'VITD'],
+      tests: %w[CHO VITD],
       price: 1700
     },
     {
       id: 'TP2',
-      tests: ['HBA1C', 'B12'],
+      tests: %w[HBA1C B12],
       price: 2100
     },
     {
       id: 'TP3',
-      tests: ['LFT', 'VITD', 'CHO'],
+      tests: %w[LFT VITD CHO],
       price: 1800
     }
   ]
+
+  def self.find_by_id(id)
+    DATA.find { |panel| panel[:id] === id.upcase }
+  end
 end
