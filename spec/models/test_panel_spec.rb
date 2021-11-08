dd14c151746b979ca746fde817f42ea3da15e2de
Shih Han Wang <shih-han.wang@boxt.co.uk>
2021-11-08 20:14:59 +0000

    Models spec updated

--- spec/models/test_panel_spec.rb
+++ spec/models/test_panel_spec.rb
@@ -4,4 +4,40 @@ describe TestPanel, type: :model do
   it 'should store the data in a DATA constant' do
     expect(TestPanel::DATA.length > 0).to eq true
   end
+  describe 'class methods' do
+    context 'find_by_id' do
+      context 'when insance is found' do
+        let(:test_panel) { TestPanel::DATA.first }
+        let(:id) { test_panel[:id] }
+
+        it 'should return instance with correct id' do
+          expect(described_class.find_by_id(id).id).to eq id
+        end
+      end
+
+      context 'when insance is not found' do
+        let(:id) { 'TPTEST' }
+
+        it 'should return instance with correct id' do
+          expect { described_class.find_by_id(id) }.to raise_error(TestPanel::NotFoundError)
+        end
+      end
+    end
+  end
+
+  describe 'instance methods' do
+    let(:test_panel) { TestPanel.new(**TestPanel::DATA.first) }
+
+    context 'sample_tube_types' do
+      it 'should return correct sample tube types' do
+        expect(test_panel.sample_tube_types).to eq %i[yellow yellow]
+      end
+    end
+
+    context 'sample_volume_requirement' do
+      it 'should return correct sample volume requirement' do
+        expect(test_panel.sample_volume_requirement).to eq 220
+      end
+    end
+  end
 end
--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -4,4 +4,23 @@ describe Test, type: :model do
   it 'should store the data in a DATA constant' do
     expect(Test::DATA.length > 0).to eq true
   end
+
+  describe 'class methods' do
+    context 'when insance is found' do
+      let(:test_model) { Test::DATA.first }
+      let(:id) { test_model[:id] }
+
+      it 'should return instance with correct id' do
+        expect(described_class.find_by_id(id).id).to eq id
+      end
+    end
+
+    context 'when insance is not found' do
+      let(:id) { 'TESTID' }
+
+      it 'should return instance with correct id' do
+        expect { described_class.find_by_id(id) }.to raise_error(Test::NotFoundError)
+      end
+    end
+  end
 end
